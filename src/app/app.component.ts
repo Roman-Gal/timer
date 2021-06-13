8b75026da2c9dec2590a2a9aef2ec7a15a136cb5
lkre <lukasz.res@gmail.com>
2021-06-13 14:49:12 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {interval, Subscription} from "rxjs";
+import {TimeModel} from "./time.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sda-timer';
+export class AppComponent implements OnInit {
+  time: TimeModel = {
+    seconds: 59,
+    minutes: 0
+  };
+  subscription?: Subscription;
+  isPaused: boolean = true;
+
+  ngOnInit(): void {
+    this.subscription = interval(1000).subscribe(() => {
+      this.countDown();
+    })
+  }
+
+  private countDown() {
+    if (!this.isPaused) {
+      if (this.time.seconds > 0) {
+        this.time.seconds -= 1;
+      } else {
+        this.time.seconds = 59;
+      }
+
+      if (this.time.minutes === 0 && this.time.seconds === 0) {
+        this.subscription?.unsubscribe();
+      }
+    }
+  }
+
+  togglePause() {
+    this.isPaused = !this.isPaused;
+  }
+
+
 }
